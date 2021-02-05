a3d7f3f2b57e8b04af97c95141c998085f2010b1
Akin <rapiddevelopa@gmail.com>
2021-02-05 19:29:03 +0100

    Initial Commit

---
+++ PinCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PinCode
+//
+//  Created by Admin on 2/5/21.
+//  Copyright © 2021 rapid interactive. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
